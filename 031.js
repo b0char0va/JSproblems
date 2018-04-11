327ae14dd4ba563baac1b5a47e58758c75ce58b8
Mona <bocharova.mona@gmail.com>
2018-04-11 15:48:25 +0100

    minor correction

--- 031.js
+++ 031.js
@@ -1,7 +1,7 @@
 function convertToRoman(num) {
     var obj = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
     var romanNum = "";
-    for ( i in obj ) {
+    for ( var i in obj ) {
         while ( num >= obj[i] ) {
             romanNum += i;
             num -= obj[i];
